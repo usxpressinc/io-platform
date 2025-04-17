f9cc79d55553ee65d42f7335974ad7e4b8cb3b2a
Vibin Daniel <10978639+vibindaniel@users.noreply.github.com>
2025-04-17 08:54:02 -0400

    [IO-112]: change all status codes to 202

--- src/endpoints/clara/router.py
+++ src/endpoints/clara/router.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Response, Security, status
+from fastapi import APIRouter, HTTPException, Response, Security, status
 
 from src.helpers.auth import authenticate_token
 from src.models.response import LLMResponse
@@ -12,7 +12,7 @@ router = APIRouter(prefix="/api/clara", include_in_schema=True, tags=["clara"])
     "/carriers/valid",
     summary="Check whether a carrier is valid using Highway API",
     response_description="Return HTTP Status Code 200 (OK)",
-    status_code=status.HTTP_200_OK,
+    status_code=status.HTTP_202_ACCEPTED,
     response_model=LLMResponse[carrier_vetting_models.CarrierValidityResponse],
 )
 def get_carrier_validity(
@@ -23,15 +23,16 @@ def get_carrier_validity(
     """
     ## Check whether a carrier is valid using Highway API
     """
-    result = carrier_vetting_service.get_carrier_validity(
-        dotNumber=item.dotNumber,
-        mcNumber=item.mcNumber,
-        brokerage_order_id=item.brokerageOrderId,
-    )
-    response.status_code = result.statusCode
-    if result.error:
-        response.status_code = status.HTTP_400_BAD_REQUEST
-
-    return LLMResponse[carrier_vetting_models.CarrierValidityResponse](
-        data=result, schema=carrier_vetting_models.schema
-    )
+    try:
+        result = carrier_vetting_service.get_carrier_validity(
+            dotNumber=item.dotNumber,
+            mcNumber=item.mcNumber,
+            brokerage_order_id=item.brokerageOrderId,
+        )
+        response.status_code = status.HTTP_202_ACCEPTED
+        return LLMResponse[carrier_vetting_models.CarrierValidityResponse](
+            data=result, schema=carrier_vetting_models.schema
+        )
+    except HTTPException as e:
+        response.status_code = status.HTTP_202_ACCEPTED
+        return e.detail
