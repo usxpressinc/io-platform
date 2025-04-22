babe2654a01a83482e6889e2cc49fe40274cb116
Vibin Daniel <10978639+vibindaniel@users.noreply.github.com>
2025-04-22 10:27:36 -0400

    [IO-122]: fix emails

--- src/endpoints/common/send_email/models.py
+++ src/endpoints/common/send_email/models.py
@@ -3,10 +3,13 @@ from pydantic import BaseModel
 
 class SendEmailResponse(BaseModel):
     status: str
+    errors: list[str] = []
 
 
 class SendEmailRequest(BaseModel):
     body: str
     from_email: str
-    to_email: str
+    to_emails: list[str]
     subject: str
+    cc_emails: list[str] = []
+    bcc_emails: list[str] = []
--- src/endpoints/common/send_email/service.py
+++ src/endpoints/common/send_email/service.py
@@ -1,7 +1,17 @@
 import logging
 
 import sendgrid
-from sendgrid.helpers.mail import Email, HtmlContent, Mail, To
+from glom import glom
+from python_http_client.exceptions import HTTPError
+from sendgrid.helpers.mail import (
+    Bcc,
+    Cc,
+    Email,
+    HtmlContent,
+    Mail,
+    Personalization,
+    To,
+)
 
 from src.settings import Settings
 
@@ -17,12 +27,30 @@ def send_email(item: models.SendEmailRequest) -> models.SendEmailResponse:
     logger.info(repr(item))
 
     from_email = Email(item.from_email)
-    to_email = To(item.to_email)
     subject = item.subject
     content = HtmlContent(item.body)
-    mail = Mail(from_email, to_email, subject, content)
-    response = sg.client.mail.send.post(request_body=mail.get())
-    logger.info(response.status_code)
-    logger.info(response.body)
-    logger.info(response.headers)
+    email = Mail(from_email=from_email, subject=subject, html_content=content)
+
+    personalization = Personalization()
+    for to_email in item.to_emails:
+        personalization.add_to(To(to_email))
+    for cc_email in item.cc_emails:
+        personalization.add_cc(Cc(cc_email))
+    for bcc_email in item.bcc_emails:
+        personalization.add_bcc(Bcc(bcc_email))
+    email.add_personalization(personalization=personalization)
+
+    try:
+        response = sg.send(email)
+        logger.info(response.status_code)
+        logger.info(response.body)
+        logger.info(response.headers)
+    except HTTPError as e:
+        logger.error(e.status_code)
+        logger.error(e.body)
+        logger.error(e.headers)
+        return models.SendEmailResponse(
+            status=str(e.status_code),
+            errors=glom(e.to_dict.get("errors"), ["message"], default=[]),
+        )
     return models.SendEmailResponse(status=str(response.status_code))
